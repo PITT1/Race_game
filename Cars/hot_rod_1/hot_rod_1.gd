8f091a732760066e36d4f1139110834ec0401e6c
PITT1 <petroleo.plta.97@gmail.com>
2025-05-01 20:14:03 -0400

    agregando control al estar en el aire

--- Cars/hot_rod_1/hot_rod_1.gd
+++ Cars/hot_rod_1/hot_rod_1.gd
@@ -2,8 +2,25 @@ extends VehicleBody3D
 
 @export var MAX_STEER = 0.7
 @export var ENGINE_POWER = 1000
+@onready var ground_sensor: RayCast3D = $ground_sensor
 
 
 func _physics_process(delta: float) -> void:
 	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 10)
 	engine_force = Input.get_axis("ui_down", "ui_up") * ENGINE_POWER
+	
+	air_control(delta)
+
+func air_control(delta):
+	if not ground_sensor.is_colliding():
+		if Input.is_action_just_pressed("ui_left"):
+			set_angular_velocity(Vector3.UP * 100 * delta)
+		
+		if Input.is_action_just_pressed("ui_right"):
+			set_angular_velocity(Vector3.DOWN * 100 * delta)
+			
+		if Input.is_action_just_pressed("ui_up"):
+			set_angular_velocity(lerp(Vector3(0,0,0), Vector3(0,0,100), delta))
+		
+		if Input.is_action_just_pressed("ui_down"):
+			set_angular_velocity(Vector3(0,0,-100) * delta)
