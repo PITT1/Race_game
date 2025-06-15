dca5144956cfd2a794834a8aa2b52339e9eeb372
PITT1 <petroleo.plta.97@gmail.com>
2025-06-14 21:53:48 -0400

    volviendo a programar al hot_rod_1

--- Cars/hot_rod_1/hot_rod_1.gd
+++ Cars/hot_rod_1/hot_rod_1.gd
@@ -1,51 +1,11 @@
 extends VehicleBody3D
 
 @export var MAX_STEER = 0.7
-@export var ENGINE_POWER = 200
-@onready var ground_sensor: RayCast3D = $ground_sensor
+@export var ENGINE_POWER = 1500
 
-@onready var wheel_1: VehicleWheel3D = $wheel_1
-@onready var wheel_2: VehicleWheel3D = $wheel_2
-@onready var wheel_3: VehicleWheel3D = $wheel_3
-@onready var wheel_4: VehicleWheel3D = $wheel_4
-
-@onready var timer_oil_trap: Timer = $Timer_oil_trap
-var oil_trap_on = false
-
+@export var is_player: bool = true
 
 func _physics_process(delta: float) -> void:
-	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 10)
-	engine_force = Input.get_axis("ui_down", "ui_up") * ENGINE_POWER
-	
-	air_control(delta)
-	
-	if oil_trap_on:
-		on_oil_trap()
-
-func air_control(delta):
-	if not ground_sensor.is_colliding():
-		if Input.is_action_pressed("ui_left"):
-			set_angular_velocity(lerp(angular_velocity, Vector3.UP * 5, delta))
-		
-		if Input.is_action_pressed("ui_right"):
-			set_angular_velocity(lerp(angular_velocity, Vector3.DOWN * 5, delta))
-			
-		if Input.is_action_pressed("ui_up"):
-			set_angular_velocity(lerp(angular_velocity, Vector3(0,0,5), delta))
-		
-		if Input.is_action_pressed("ui_down"):
-			set_angular_velocity(lerp(angular_velocity, Vector3(0,0,-5), delta))
-
-func on_oil_trap():
-	timer_oil_trap.start()
-	wheel_1.set_friction_slip(0.5)
-	wheel_2.set_friction_slip(0.5)
-	wheel_3.set_friction_slip(0.5)
-	wheel_4.set_friction_slip(0.5)
-
-
-func _on_timer_oil_trap_timeout() -> void:
-	wheel_1.set_friction_slip(5)
-	wheel_2.set_friction_slip(5)
-	wheel_3.set_friction_slip(5)
-	wheel_4.set_friction_slip(5)
+	if is_player:
+		steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 10)
+		engine_force = Input.get_axis("ui_down", "ui_up") * ENGINE_POWER
