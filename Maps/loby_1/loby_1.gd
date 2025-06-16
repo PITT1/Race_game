62a7c0191d869a80ce2ddf9ca905477759ed1da0
PITT1 <petroleo.plta.97@gmail.com>
2025-06-16 03:09:32 -0400

    creando pantalla de seleccion de vehiculos

--- Maps/loby_1/loby_1.gd
+++ Maps/loby_1/loby_1.gd
@@ -1,3 +1,9 @@
 extends Node3D
 
 var num_checkpoints: int = 0
+
+func _ready() -> void:
+	var childs = get_children()
+	for item in childs:
+		if item.get_class() == "VehicleBody3D":
+			item.is_on_race = true
--- huds/loby_menus/to_race_menu.gd
+++ huds/loby_menus/to_race_menu.gd
@@ -13,3 +13,11 @@ func output_anim():
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "output":
 		queue_free()
+
+
+func _on_to_f_1_race_button_up() -> void:
+	print("ir al circuito f1")
+
+
+func _on_to_rc_circuit_button_up() -> void:
+	get_tree().change_scene_to_file("res://Maps/rc_circuit/rc_circuit_1.tscn")
