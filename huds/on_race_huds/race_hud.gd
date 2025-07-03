78e7fb71d218d86ce7e5411aeb90c0bf696be303
PITT1 <petroleo.plta.97@gmail.com>
2025-07-02 21:54:57 -0400

    hud para el sistema de clasificacion

---
+++ huds/on_race_huds/race_hud.gd
@@ -0,0 +1,18 @@
+extends Control
+@onready var position_label: Label = $CanvasLayer/position_label
+
+var car: VehicleBody3D = null
+@onready var race_list: Array = []
+var label_text: String = ""
+
+func _ready() -> void:
+	car = get_parent()
+
+func _physics_process(delta: float) -> void:
+	race_list = get_parent().get_parent().race_order
+	
+	for i in race_list.size():
+		if race_list[i] == car:
+			label_text = "pos: " + str(i + 1)
+			position_label.set_text(label_text)
+			
