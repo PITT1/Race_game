a37b2b80e8f2d90987b75b0bc82ad30149a14df0
PITT1 <petroleo.plta.97@gmail.com>
2025-07-04 23:16:35 -0400

    mejorando el race_hud

--- BOT_sistem/BOTS_for_race.gd
+++ BOT_sistem/BOTS_for_race.gd
@@ -86,7 +86,7 @@ func _on_checkpoint_sensor_area_entered(area: Area3D) -> void:
 		print(name," lap: ", laps_num)
 		checkpoint_store = ["point_0"]
 		
-		if laps_num >= get_parent().laps_num_to_finish:
+		if laps_num >= get_parent().laps_num_to_finish + 1:
 			print("FIN DE LA CARRERA")
 			is_on_race = false
 			engine_force = 0
--- huds/on_race_huds/race_hud.gd
+++ huds/on_race_huds/race_hud.gd
@@ -1,9 +1,12 @@
 extends Control
 @onready var position_label: Label = $CanvasLayer/position_label
+@onready var lap_label: Label = $CanvasLayer/lap_label
 
 var car: VehicleBody3D = null
 @onready var race_list: Array = []
 var label_text: String = ""
+var previous_lap: int = 0
+var previous_pos: int = 0
 
 func _ready() -> void:
 	car = get_parent()
@@ -11,12 +14,27 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if delta:
 		pass
+	
+	show_position()
+	
+	show_laps()
+	
+
+func show_position():
 	if get_parent().get_parent().is_race == true:
 		race_list = get_parent().get_parent().race_order
 		
 		for i in race_list.size():
 			if race_list[i] == car:
-				label_text = "pos: " + str(i + 1)
-				position_label.set_text(label_text)
+				if i != previous_pos:
+					label_text = "pos: " + str(i + 1)
+					position_label.set_text(label_text)
+					previous_pos = i
 	else:
 		position_label.visible = false
+		lap_label.visible = false
+
+func show_laps():
+	if car.laps_num != previous_lap:
+		lap_label.text = "lap: " + str(car.laps_num)
+		previous_lap = car.laps_num
