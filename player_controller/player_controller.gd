97ea6422cfb163e0721cc53fe353ea15a2eb06ab
PITT1 <petroleo.plta.97@gmail.com>
2025-07-05 11:00:38 -0400

    mejorando el movimiento de la camara

--- player_controller/player_controller.gd
+++ player_controller/player_controller.gd
@@ -6,6 +6,13 @@ var is_on_race = false
 var dificulty: int = 0
 var min_distance_to_point: int = 0
 var poitn_desviation: int = 0
+var pCam: PhantomCamera3D = null
+
+@export_category("CAMERA")
+@export var camera_distance: float = 10
+@export var camera_height: float = 5
+var target_offset = Vector2.ZERO
+var current_offset = Vector2.ZERO
 
 
 @export var is_player: bool = true
@@ -18,13 +25,17 @@ var laps_num: int = 1
 
 func _ready() -> void:
 	all_checkpoints = get_parent().num_checkpoints #esto hay que cambiarlo para el futuro ya que no se podra entrar al loby ni eventos de destruccion
-
+	var siblings = get_parent().get_children()
+	for item in siblings:
+		if item.name == "PhantomCamera3D":
+			pCam = item
 
 func _physics_process(delta: float) -> void:
 	if is_player and is_on_race:
 		steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 10)
 		engine_force = Input.get_axis("ui_down", "ui_up") * ENGINE_POWER
-		
+	
+	update_pcam()
 	
 
 func _on_checkpoint_sensor_area_entered(area: Area3D) -> void:
@@ -45,4 +56,9 @@ func _on_checkpoint_sensor_area_entered(area: Area3D) -> void:
 
 func start_race():
 	is_on_race = true
-	
+
+func update_pcam():
+	var forward_direction = -global_transform.basis.z.normalized()
+	target_offset = Vector2(forward_direction.x * camera_distance, forward_direction.z * camera_distance)
+	current_offset = current_offset.lerp(target_offset, 0.02)
+	pCam.follow_offset = Vector3(current_offset.x, camera_height, current_offset.y)
