b73082cec24bb0fd65beb21b6ca316210d055f66
PITT1 <petroleo.plta.97@gmail.com>
2025-07-12 02:17:47 -0400

    agregando sistema de recompenzas al final de la carrera

--- huds/on_race_huds/on_finish_race_hud.gd
+++ huds/on_race_huds/on_finish_race_hud.gd
@@ -2,7 +2,7 @@ extends Control
 @onready var num_finished_label: Label = $CanvasLayer/VBoxContainer/num_finished_label
 @onready var reward_label: Label = $CanvasLayer/VBoxContainer/reward_label
 @onready var back_btn: Button = $CanvasLayer/VBoxContainer/HBoxContainer/back_btn
-
+@export var reward_multiplier: Array = []
 
 func set_finish_result(position: int, reward):
 	var pos_str: Array = ["ro", "st", "nd", "rd", "th"]
@@ -10,9 +10,10 @@ func set_finish_result(position: int, reward):
 		num_finished_label.set_text(str(position) + pos_str[position])
 	else:
 		num_finished_label.set_text(str(position) + pos_str[4])
-		
-	reward_label.set_text("You reward: " + str(reward))
-
+	if position < 4:
+		reward_label.set_text("You reward: " + str(int(reward * reward_multiplier[position - 1])))
+	else:
+		reward_label.set_text("You reward: " + str(int(reward * reward_multiplier[4])))
 
 func _on_back_btn_button_up() -> void:
 	get_tree().change_scene_to_file("res://Maps/loby_1/loby_1.tscn")
