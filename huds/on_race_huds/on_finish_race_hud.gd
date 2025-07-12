b55f7a43fd49f842605704079320585d45e5bfd0
PITT1 <petroleo.plta.97@gmail.com>
2025-07-12 02:29:24 -0400

    arreglando error en sistema de recompensas

--- huds/on_race_huds/on_finish_race_hud.gd
+++ huds/on_race_huds/on_finish_race_hud.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var reward_label: Label = $CanvasLayer/VBoxContainer/reward_label
 @onready var back_btn: Button = $CanvasLayer/VBoxContainer/HBoxContainer/back_btn
 @export var reward_multiplier: Array = []
+var reward_to_save: int
+var global_var = load("res://global_var.gd").new()
 
 func set_finish_result(position: int, reward):
 	var pos_str: Array = ["ro", "st", "nd", "rd", "th"]
@@ -11,9 +13,16 @@ func set_finish_result(position: int, reward):
 	else:
 		num_finished_label.set_text(str(position) + pos_str[4])
 	if position < 4:
-		reward_label.set_text("You reward: " + str(int(reward * reward_multiplier[position - 1])))
+		reward_to_save = int(reward * reward_multiplier[position - 1])
+		reward_label.set_text("You reward: " + str(reward_to_save))
 	else:
-		reward_label.set_text("You reward: " + str(int(reward * reward_multiplier[4])))
+		reward_to_save = int(reward * reward_multiplier[4])
+		reward_label.set_text("You reward: " + str(reward_to_save))
+	
+	var data = JSON.parse_string(global_var.load_data())
+	data.money += reward_to_save
+	global_var.save_data(JSON.stringify(data))
+	
 
 func _on_back_btn_button_up() -> void:
 	get_tree().change_scene_to_file("res://Maps/loby_1/loby_1.tscn")
