2192f98be88933fa1118aefb34bff8deb5b386d2
PITT1 <petroleo.plta.97@gmail.com>
2025-07-16 04:28:04 -0400

    mostrando dinero total en el loby

---
+++ huds/loby_menus/loby_hud.gd
@@ -0,0 +1,11 @@
+extends Control
+var global_var = load("res://global_var.gd").new()
+@onready var money_label: Label = $CanvasLayer/money_label
+var real_part: Array
+
+func _ready() -> void:
+	var data = JSON.parse_string(global_var.load_data())
+	var str_money: String = str(data.money) 
+	if str_money.contains("."):
+		real_part = str_money.split(".")
+		money_label.text = str(real_part[0])
