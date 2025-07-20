1bb0bca85c359df6ec81c930e31c350c303f35bf
PITT1 <petroleo.plta.97@gmail.com>
2025-07-19 22:34:12 -0400

    mejorando controles tactiles

---
+++ huds/controls_hud/tactil_control_1.gd
@@ -0,0 +1,39 @@
+extends Control
+@onready var brake_reverse: TouchScreenButton = $CanvasLayer/brake_reverse
+@onready var acelerate: TouchScreenButton = $CanvasLayer/acelerate
+@onready var left: TouchScreenButton = $CanvasLayer/left
+@onready var right: TouchScreenButton = $CanvasLayer/right
+
+var color_presed = Color(1, 1, 1, 0.60)
+var color_release = Color(1, 1, 1, 0.39)
+
+func _on_brake_reverse_pressed() -> void:
+	brake_reverse.modulate = color_presed
+
+
+func _on_brake_reverse_released() -> void:
+	brake_reverse.modulate = color_release
+
+
+func _on_acelerate_pressed() -> void:
+	acelerate.modulate = color_presed
+
+
+func _on_acelerate_released() -> void:
+	acelerate.modulate = color_release
+
+
+func _on_left_pressed() -> void:
+	left.modulate = color_presed
+
+
+func _on_left_released() -> void:
+	left.modulate = color_release
+
+
+func _on_right_pressed() -> void:
+	right.modulate = color_presed
+
+
+func _on_right_released() -> void:
+	right.modulate = color_release
