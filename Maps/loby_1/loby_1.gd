0f88a9882b821ed37e6c2a6828806c5de79ffdaf
PITT1 <petroleo.plta.97@gmail.com>
2025-07-20 01:51:06 -0400

    mejorando la pantalla de inicio

--- Maps/loby_1/loby_1.gd
+++ Maps/loby_1/loby_1.gd
@@ -1,13 +1,17 @@
 extends Node3D
 @onready var pCam: PhantomCamera3D = $PhantomCamera3D
+@onready var main_hud_cam: PhantomCamera3D = $main_hud_cam
 
 var num_checkpoints: int = 0
 var global_var = load("res://global_var.gd").new()
+const LOBY_HUD = preload("res://huds/loby_menus/loby_hud.tscn")
+const TACTIL_CONTROL_1 = preload("res://huds/controls_hud/tactil_control_1.tscn")
 var is_race = false
+var on_play: bool = false
+var initialized: bool = false
 
 func _ready() -> void:
-	physics_interpolation_mode = Node.PHYSICS_INTERPOLATION_MODE_ON
-	
+	main_hud_cam.priority_override = true
 	var data = JSON.parse_string(global_var.load_data())
 	var car_name = data.car
 	var car_adrees = global_var.car_list[car_name]
@@ -16,10 +20,23 @@ func _ready() -> void:
 	var car_instantia = car_selected.instantiate()
 	add_child(car_instantia)
 	car_instantia.global_position = Vector3(0, 5, 0)
+	car_instantia.brake = 100
 	pCam.follow_target = car_instantia
 	pCam.look_at_target = car_instantia
-	
-	var childs = get_children()
-	for item in childs:
-		if item.get_class() == "VehicleBody3D":
-			item.is_on_race = true
+
+func _physics_process(delta: float) -> void:
+	if delta:
+		pass
+	if on_play and not initialized:
+		var childs = get_children()
+		for item in childs:
+			if item.get_class() == "VehicleBody3D":
+				item.is_on_race = true
+		
+		var hud_intantia = LOBY_HUD.instantiate()
+		add_child(hud_intantia)
+		
+		var controls_instantia = TACTIL_CONTROL_1.instantiate()
+		add_child(controls_instantia)
+		
+		initialized = true
--- huds/loby_menus/main_hud.gd
+++ huds/loby_menus/main_hud.gd
@@ -1,5 +1,17 @@
 extends Control
+@onready var anim: AnimationPlayer = $AnimationPlayer
 
 
 func _on_quit_btn_button_up() -> void:
 	get_tree().quit()
+
+
+func _on_play_btn_button_up() -> void:
+	get_parent().on_play = true
+	get_parent().pCam.priority = 2
+	anim.play("out")
+	
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "out":
+		queue_free()
